e493a737a43c337a604afcaaf144f38697def3c4
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-09 18:05:26 -0400

    routes for notes.html and index.html added

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const api = require("./db/db.json");
+const database = require("./db/db.json");
 
+// set up the app and port
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public/index.html"));
-// app.use(express.static("public/notes.html"));
 
-app.get("/", (req, res) => {
+// routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/");
+app.get("/api/notes", async (req, res) => {
+  res.json(database);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT http://localhost:${PORT}`);
