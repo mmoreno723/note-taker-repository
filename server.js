7be1b5d81012a6cd1468d45550bd96834a54183b
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-09 22:28:04 -0400

    /api/notes get and post routes created

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db.json");
+const notes = require("express").Router();
+const { readAndAppend, readFromFile } = require("./helpers/fsUtils");
 
 // set up the app and port
 const PORT = process.env.PORT || 3001;
@@ -12,17 +14,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public/index.html"));
+app.use(express.static("public/notes.html"));
 
 // routes
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", async (req, res) => {
-  res.json(database);
+app.get("/api/notes", (req, res) => {
+  console.log(`${req.method} request recieved to get a note`);
+
+  return res.json(database);
+});
+app.post("/api/notes", (req, res) => {
+  console.log(`${req.method} request recieved to add a note`);
+
+  let response;
+
+  if (req.body) {
+    response = {
+      status: "Success",
+      data: req.body,
+    };
+    res.status(201).json(response);
+  } else {
+    res.status(400).json("Request body is missing");
+  }
+  console.log(req.body);
+
+  //   const { title, text } = req.body;
+
+  //   if (title && text) {
+  //     const newNote = {
+  //       title,
+  //       text,
+  //     };
+
+  //     const response = {
+  //       status: "Success, note added",
+  //       body: newNote,
+  //     };
+
+  //     console.log(response);
+  //     res.status(201).json(response);
+  //   } else {
+  //     res.status(500).json("Error in adding note");
+  //   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
